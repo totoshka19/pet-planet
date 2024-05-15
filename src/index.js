518ff4bf40ea12b92e47dc12157a2e2e95283d6a
Anna Kapitanova <totoshechka19@gmail.com>
2024-05-15 19:44:32 +0300

    Получение домиков с сервера

--- src/index.js
+++ src/index.js
@@ -1,15 +1,62 @@
+const API_URL = 'https://flicker-lean-hardhat.glitch.me';
+// /api/products/category
+
+const productList = document.querySelector('.store__list');
+
+const createProductCard = (product) => {
+  console.log(product);
+  const productCard = document.createElement('li');
+  productCard.classList.add('store__item');
+  productCard.innerHTML = `
+    <article class="store__product product">
+      <img class="product__image" src="${API_URL}${product.photoUrl}" alt="${product.name}" width="388" height="261">
+      <h3 class="product__title">${product.name}</h3>
+      <p class="product__price">${product.price}&nbsp;₽</p>
+      <button class="product__button-add-cart">Заказать</button>
+    </article>
+  `;
+
+  return productCard;
+};
+
+const renderProducts = (products) => {
+  productList.textContent = '';
+  products.forEach((product) => {
+    const productCard = createProductCard(product);
+    productList.append(productCard);
+  });
+};
+
+const fetchProductByCategory = async (category) => {
+  try {
+    const response = await fetch(`${API_URL}/api/products/category/${category}`);
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    const products = await response.json();
+    renderProducts(products);
+
+  } catch (error) {
+      console.error(`Ошибка запроса товаров: ${error}`);
+  }
+};
+
+fetchProductByCategory("Домики");
+
 const buttons = document.querySelectorAll('.store__category-button');
 
 const changeActiveBtn = (event) => {
-    const target = event.target;
+  const target = event.target;
 
-    buttons.forEach((button) => {
-        button.classList.remove('store__category-button_active');
-    });
+  buttons.forEach((button) => {
+      button.classList.remove('store__category-button_active');
+  });
 
-    target.classList.add('store__category-button_active');
+  target.classList.add('store__category-button_active');
 };
 
 buttons.forEach((button) => {
-    button.addEventListener('click', changeActiveBtn)
+  button.addEventListener('click', changeActiveBtn)
 });
