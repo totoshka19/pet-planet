8dbaef76ef2b3efb61963018be353be95b64d91c
Anna Kapitanova <totoshechka19@gmail.com>
2024-05-20 17:50:50 +0300

    Доделала функционал оформления заказа

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const API_URL = 'https://flicker-lean-hardhat.glitch.me';
-// /api/products/category
+
 const buttons = document.querySelectorAll('.store__category-button');
 const productList = document.querySelector('.store__list');
 const cartButton = document.querySelector('.store__cart-button');
@@ -7,8 +7,26 @@ const cartCount = cartButton.querySelector('.store__cart-count');
 const modalOverlay = document.querySelector('.modal-overlay');
 const cartItemsList = modalOverlay.querySelector('.modal__cart-items');
 const modalCloseButton =  modalOverlay.querySelector('.modal-overlay_close-button');
+const cartTotalPriceElement = modalOverlay.querySelector('.modal__cart-price');
+const cartForm = modalOverlay.querySelector('.modal__cart-form');
 
-const createProductCard = ({ photoUrl, name, price }) => {
+const orderMessageElement = document.createElement('div');
+orderMessageElement.classList.add('order-message');
+
+const orderMessageText= document.createElement('p');
+orderMessageText.classList.add('order-message__text');
+
+const orderMessageTCloseButton= document.createElement('button');
+orderMessageTCloseButton.classList.add('order-message__close-button');
+orderMessageTCloseButton.textContent = 'Закрыть';
+
+orderMessageElement.append(orderMessageText, orderMessageTCloseButton);
+
+orderMessageTCloseButton.addEventListener('click', () => {
+  orderMessageElement.remove();
+});
+
+const createProductCard = ({ id, photoUrl, name, price }) => {
   const productCard = document.createElement('li');
   productCard.classList.add('store__item');
   productCard.innerHTML = `
@@ -16,7 +34,7 @@ const createProductCard = ({ photoUrl, name, price }) => {
       <img class="product__image" src="${API_URL}${photoUrl}" alt="${name}" width="388" height="261">
       <h3 class="product__title">${name}</h3>
       <p class="product__price">${price}&nbsp;₽</p>
-      <button class="product__button-add-cart">Заказать</button>
+      <button class="product__button-add-cart" data-id="${id}">Заказать</button>
     </article>
   `;
 
@@ -47,6 +65,21 @@ const fetchProductByCategory = async (category) => {
   }
 };
 
+const fetchCartItems = async (ids) => {
+  try {
+    const response = await fetch(`${API_URL}/api/products/list/${ids.join(',')}`);
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.error(`Ошибка запроса товаров для корзины: ${error}`);
+    return [];
+  }
+};
+
 const changeCategory = ({target}) => {
   const category = target.textContent;
 
@@ -66,19 +99,61 @@ buttons.forEach((button) => {
   }
 });
 
-const renderCartItems = () => {
+const calculateTotalPrice = (cartItems, products) => cartItems.reduce((acc,item) => {
+  const product = products.find((prod) => prod.id === item.id);
+  return acc + product.price * item.count;
+}, 0);
+
+const renderCartItems = async () => {
   cartItemsList.textContent = '';
   const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+  const products = JSON.parse(localStorage.getItem('cartProductDetails') || '[]');
 
-  cartItems.forEach(item => {
+  products.forEach(({ id, photoUrl, name, price }) => {
+    const cartItem = cartItems.find(item => item.id === id);
+    if (!cartItem) {
+      return;
+    }
+
     const listItem = document.createElement('li');
-    listItem.textContent = item;
+    listItem.classList.add('modal__cart-item');
+    listItem.innerHTML = `
+      <img class="modal__cart-item-image" src="${API_URL}${photoUrl}" alt="${name}">
+  
+      <h3 class="modal__cart-item-title">${name}</h3>
+      
+      <div class="modal__cart-item-count">
+      <button class="modal__btn modal__minus" data-id="${id}">-</button>
+      <span class="modal__count">${cartItem.count}</span>
+      <button class="modal__btn modal__plus" data-id="${id}">+</button>
+      </div>
+      
+      <p class="modal__cart-item-price">${price * cartItem.count}&nbsp;₽</p>
+    `;
+
     cartItemsList.append(listItem);
   });
+
+  const totalPrice = calculateTotalPrice(cartItems, products);
+  cartTotalPriceElement.innerHTML = `${totalPrice}&nbsp;₽`;
 };
 
-cartButton.addEventListener('click', () => {
+cartButton.addEventListener('click', async () => {
   modalOverlay.style.display = 'flex';
+
+  const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+  const ids = cartItems.map(item => item.id);
+
+  if (!ids.length) {
+    cartItemsList.textContent = '';
+    const listItem = document.createElement('li');
+    listItem.textContent = 'Корзина пуста';
+    cartItemsList.append(listItem);
+    return;
+  }
+
+  const products = await fetchCartItems(ids);
+  localStorage.setItem('cartProductDetails', JSON.stringify(products));
   renderCartItems();
 });
 
@@ -98,18 +173,100 @@ const updateCartCount = () => {
 
 updateCartCount();
 
-const addToCart = (productName) => {
+const addToCart = (productId) => {
   const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
-  cartItems.push(productName);
+
+  const existingItem = cartItems.find((item) => item.id === productId);
+
+  if (existingItem) {
+    existingItem.count += 1;
+  } else {
+    cartItems.push({id: productId, count: 1});
+  }
+
   localStorage.setItem('cartItems', JSON.stringify(cartItems));
   updateCartCount();
 };
 
 productList.addEventListener('click', ({target}) => {
   if (target.closest('.product__button-add-cart')) {
-    const productCard = target.closest('.store__product');
-    const productName = productCard.querySelector('.product__title').textContent;
-    console.log(productName)
-    addToCart(productName);
+    const productId = target.dataset.id;
+    addToCart(productId);
   }
 });
+
+const updateCartItem = (productId, change)=> {
+  const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+  const itemIndex = cartItems.findIndex(item => item.id === productId);
+
+  if (itemIndex !== -1) {
+    cartItems[itemIndex].count += change;
+
+    if (cartItems[itemIndex].count <= 0) {
+      cartItems.splice(itemIndex, 1)
+    }
+
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
+
+    updateCartCount();
+    renderCartItems();
+  }
+};
+
+cartItemsList.addEventListener('click',({target}) => {
+  if (target.classList.contains('modal__plus')) {
+    const productId = target.dataset.id;
+    updateCartItem(productId, 1);
+  }
+
+  if (target.classList.contains('modal__minus')) {
+    const productId = target.dataset.id;
+    updateCartItem(productId, -1);
+  }
+});
+
+const submitOrder = async (e) => {
+  e.preventDefault();
+
+  const storeId = cartForm.store.value;
+  const cartItems = JSON.parse(localStorage.getItem('cartItems') || '[]');
+
+  const products = cartItems.map(({id, count}) => ({
+    id,
+    quantity: count,
+  }));
+
+  try {
+    const response = await fetch(`${API_URL}/api/orders`, {
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ storeId, products }),
+    });
+
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+
+    localStorage.removeItem('cartItems');
+    localStorage.removeItem('cartProductDetails');
+
+    const { orderId } = await response.json();
+
+    let tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    let tomorrowFormatted = tomorrow.toLocaleDateString('ru-RU', { day: 'numeric', month: 'long' });
+
+    orderMessageText.textContent = `Ваш заказ оформлен, номер ${orderId}. Вы можете его забрать завтра ${tomorrowFormatted} после 12:00`;
+    document.body.append(orderMessageElement);
+
+    modalOverlay.style.display = 'none';
+    updateCartCount();
+
+  } catch (error) {
+    console.error(`Ошибка оформления заказа: ${error}`);
+  }
+};
+
+cartForm.addEventListener('submit', submitOrder);
